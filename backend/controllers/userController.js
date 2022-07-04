fc7f96901f40947b3d6662e8a21e2a462b19a327
Jared Silver <jrsilver88@gmail.com>
2022-07-03 23:55:45 -0700

    created auth middleware for routes, create a personal profile protected route, generated a jsonwebtoken for each created user.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,8 @@
 import asyncHandler from 'express-async-handler'
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+
+import User from '../models/userModel.js'
 
 //* Register a new user
 //* /api/users
@@ -10,13 +14,78 @@ export const registerUser = asyncHandler(async (req, res) => {
     res.status(400)
     throw new Error('Please include all fields')
   }
+
+  //   Find if user already exists
+  const userExists = await User.findOne({ email })
+
+  if (userExists) {
+    res.status(400)
+    throw new Error('User already exists')
+  }
+
+  //   Hash password
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password, salt)
+
+  //   Create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  })
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid user data')
+  }
 })
 
 //* Login a user
 //* /api/users/login
 //* access public
 export const loginUser = asyncHandler(async (req, res) => {
-  res.send('Login User')
+  const { email, password } = req.body
+
+  const user = await User.findOne({ email })
+
+  //   Check user and passwords match
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    })
+  } else {
+    res.status(401)
+    throw new Error('Invalid credentials')
+  }
 })
+
+//* get current user
+//* /api/users/me
+//* access private
+export const getMe = asyncHandler(async (req, res) => {
+  const user = {
+    id: req.user._id,
+    email: req.user.email,
+    name: req.user.name,
+  }
+  res.status(200).json(user)
+})
+
+// Generate Token
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '365d',
+  })
+}
 
 export default { registerUser, loginUser }
